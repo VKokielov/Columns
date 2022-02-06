782874142d51c142502f42127ce232133e70c133
vkokielov <vkokielov@gmail.com>
2022-02-05 21:02:39 -0500

    level hash map

--- Columns/LeveledHashmap.h
+++ Columns/LeveledHashmap.h
@@ -13,17 +13,43 @@ namespace geng::lib
 	template<typename ValueType,
 		typename FirstKey,
 		typename ... RestKeys>
+	class LeveledHashmap;
+
+	template<typename ValueType,
+		     typename ... OtherTypes>
+	struct LevelElementType
+	{
+		using type = LeveledHashmap<ValueType, OtherTypes...>;
+	};
+
+	template<typename ValueType>
+	struct LevelElementType<ValueType>
+	{
+		using type = ValueType;
+	};
+
+	template<typename ValueType, typename ...OtherTypes>
+	using LevelElementType_t =
+		typename LevelElementType<ValueType, OtherTypes...>::type;
+
+	template<typename ValueType,
+		typename FirstKey,
+		typename ... RestKeys>
 	class LeveledHashmap
 	{
 	public:
-		constexpr bool isLeaf = sizeof...(RestKeys) == 0;
-		using TElement = std::conditional_t < isLeaf,
-			ValueType,
-			LeveledHashMap<ValueType, RestKeys...>;
+		static constexpr bool isLeaf = sizeof...(RestKeys) == 0;
+		using TElement = LevelElementType_t<ValueType, RestKeys...>;
+
+		using TLevelKey = FirstKey;
 		using TValue = ValueType;
 		using TIter = typename std::unordered_map<FirstKey, TElement>::iterator;
 
-		LeveledHasmap() = default;
+		LeveledHashmap() = default;
+
+		LeveledHashmap(std::size_t depth)
+			:m_depth(depth)
+		{ }
 
 		// Insert, remove, find, iterate
 
@@ -52,6 +78,11 @@ namespace geng::lib
 		}
 
 		// Find
+		auto Find(const FirstKey& key)
+		{
+			return m_childMap.find(key);
+		}
+
 		auto Find(const FirstKey& key) const
 		{
 			return m_childMap.find(key);
@@ -63,6 +94,11 @@ namespace geng::lib
 			return std::begin(m_childMap);
 		}
 
+		auto Begin() const
+		{
+			return std::cbegin(m_childMap);
+		}
+
 		auto CBegin() const
 		{
 			return std::cbegin(m_childMap);
@@ -70,21 +106,23 @@ namespace geng::lib
 
 		auto End()
 		{
-			return std::begin(m_childMap);
+			return std::end(m_childMap);
+		}
+
+		auto End() const
+		{
+			return std::cend(m_childMap);
 		}
 
 		auto CEnd() const
 		{
-			return std::cbegin(m_childMap);
+			return std::cend(m_childMap);
 		}
 
 		std::size_t GetDepth() const { return m_depth; }
 		bool IsEmpty() const { return m_childMap.empty(); }
 
 	private:
-		LeveledHashmap(std::size_t depth)
-			:m_depth(depth)
-		{ }
 
 		std::unordered_map<FirstKey, TElement> m_childMap;
 		std::size_t m_depth{ 0 };
@@ -95,104 +133,123 @@ namespace geng::lib
 	// Find
 	// It's not possible to return an iterator here, since "begin" and "end"
 	// are ill-defined when there are many maps
-	template<typename Lvl, typename K>
-	typename Lvl::TValue* Find(const Lvl& lvl, const K& key)
+	namespace map_helpers
 	{
-		const auto& levelKey = GetLevelKey(key, lvl.GetDepth());
-		if constexpr (Lvl::isLeaf)
+		template<typename Lvl, typename K, size_t depth = 0>
+		std::conditional_t<std::is_const_v<Lvl>,
+			const typename Lvl::TValue*,
+			typename Lvl::TValue*> Find(Lvl& lvl, const K& key)
 		{
-			auto levelIter = lvl.Find(levelKey);
-			if (levelIter != lvl.End())
-			{
-				return &levelIter->second;
-			}
-		}
-		else
-		{
-			// "Linear recursion"
-			auto itNextLevel = lvl.Find(levelKey);
-			if (itNextLevel != lvl.End())
-			{
-				return Find(itNextLevel->second, key);
-			}
-		}
+			auto&& levelKey = K::Getter<depth>::Get(key);
 
-		return nullptr;
-	}
-
-	// Insert. 
-	template<typename Lvl, typename K>
-	auto Insert(Lvl& lvl, const K& key)
-	{
-		const auto& levelKey = GetLevelKey(key, lvl.GetDepth());
-		
-		if constexpr (Lvl::isLeaf)
-		{
-			return lvl.Insert(levelKey);
-		}
-		else
-		{
-			auto insNextLevel = lvl.Insert(levelKey);
-			// Regardless of whether the level was inserted or already exists, call recursively
-			return Insert(insNextLevel.first->second, key);
-		}
-	}
-
-	// Remove
-	// if eraseEmpty is true, higher levels with no elements are erased
-	template<typename Lvl, typename K>
-	auto Erase(Lvl& lvl, const K& key, bool eraseEmpty)
-	{
-		const auto& levelKey = GetLevelKey(key, lvl.GetDepth());
-
-		if constexpr (Lvl::isLeaf)
-		{
-			return lvl.Erase(levelKey);
-		}
-		else
-		{
-			auto itNextLevel = lvl.Find(levelKey);
-
-			if (itNextLevel != lvl.End())
-			{
-				size_t elementsErased = Erase(itNextLevel->second, key, eraseEmpty);
-
-				if (eraseEmpty
-					&& elementsErased > 0
-					&& itNextLevel->second.IsEmpty())
-				{
-					lvl.Erase(itNextLevel);
-				}
-
-				return elementsErased;
-			}
-		}
-
-		return 0;
-	}
-
-
-	// Iterate
-	template<typename Lvl, typename F, typename ... Keys>
-	void IterateChildren(const Lvl& lvl, F&& func, Keys&& ...keys)
-	{
-		for (auto itLevel = lvl.Begin(); itLevel != lvl.End(); ++itLevel)
-		{
-			const auto& levelKey = itLevel->first;
 
 			if constexpr (Lvl::isLeaf)
 			{
-				func(std::forward<Keys>(keys)..., levelKey, itLevel->second);
+				auto levelIter = lvl.Find(levelKey);
+				if (levelIter != lvl.End())
+				{
+					return &levelIter->second;
+				}
 			}
 			else
 			{
-				// Recursion again
-				IterateChildren(itLevel->second,
-					std::forward<F>(func),
-					std::forward<Keys>(keys)...,
-					levelKey);
+				// Using-type declaration for next level
+				using TNextLevel
+					= std::conditional_t < std::is_const_v<Lvl>,
+					const typename Lvl::TElement,
+					typename Lvl::TElement>;
+
+				// "Linear recursion"
+				auto itNextLevel = lvl.Find(levelKey);
+				if (itNextLevel != lvl.End())
+				{
+					return Find<TNextLevel, K, depth + 1>(itNextLevel->second, key);
+				}
 			}
 
+			return nullptr;
+		}
+
+		// Insert. 
+		template<typename Lvl, typename K, size_t depth = 0>
+		auto Insert(Lvl& lvl, const K& key)
+		{
+			auto&& levelKey = K::Getter<depth>::Get(key);
+
+			if constexpr (Lvl::isLeaf)
+			{
+				return lvl.Insert(levelKey);
+			}
+			else
+			{
+				auto insNextLevel = lvl.Insert(levelKey);
+				// Regardless of whether the level was inserted or already exists, call recursively
+				return Insert<decltype(insNextLevel.first->second), K, depth + 1>(insNextLevel.first->second, key);
+			}
+		}
+
+		// Remove
+		// if eraseEmpty is true, higher levels with no elements are erased
+		template<typename Lvl, typename K, size_t depth = 0>
+		auto Erase(Lvl& lvl, const K& key, bool eraseEmpty)
+		{
+			auto&& levelKey = K::Getter<depth>::Get(key);
+
+			if constexpr (Lvl::isLeaf)
+			{
+				return lvl.Erase(levelKey);
+			}
+			else
+			{
+				auto itNextLevel = lvl.Find(levelKey);
+
+				if (itNextLevel != lvl.End())
+				{
+					size_t elementsErased = Erase<decltype(itNextLevel->second), K, depth + 1>(itNextLevel->second, key, eraseEmpty);
+
+					if (eraseEmpty
+						&& elementsErased > 0
+						&& itNextLevel->second.IsEmpty())
+					{
+						lvl.Erase(itNextLevel);
+					}
+
+					return elementsErased;
+				}
+			}
+
+			return 0;
+		}
+
+
+		// Iterate
+		template<typename Lvl, typename F, typename ... Keys>
+		bool IterateChildren(const Lvl& lvl, F&& func, Keys&& ...keys)
+		{
+			for (auto itLevel = lvl.Begin(); itLevel != lvl.End(); ++itLevel)
+			{
+				const auto& levelKey = itLevel->first;
+
+				if constexpr (Lvl::isLeaf)
+				{
+					if (!func(std::forward<Keys>(keys)..., levelKey, itLevel->second))
+					{
+						return false;
+					}
+				}
+				else
+				{
+					// Recursion again
+					if (!IterateChildren(itLevel->second,
+						std::forward<F>(func),
+						std::forward<Keys>(keys)...,
+						levelKey))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
 		}
 	}
 
--- Columns/UITest.cpp
+++ Columns/UITest.cpp
@@ -1,9 +1,75 @@
 #include "UIImplDatumValue.h"
 #include "UIImplDatumValueSet.h"
+#include "LeveledHashmap.h"
 
 geng::ui::impl::UIDatumValue<std::string>  myString(nullptr, 1);
 
 void TestF()
 {
 	geng::ui::impl::UIDatumValueSet<std::string> mySet{ nullptr, 1 };
+}
+
+struct TestKey
+{
+	int intKey1;
+	int intKey2;
+	std::string strKey;
+
+	template<size_t depth>
+	struct Getter;
+
+	template<>
+	struct Getter<0>
+	{
+		static int Get(const TestKey& key)
+		{
+			return key.intKey1;
+		}
+	};
+
+	template<>
+	struct Getter<1>
+	{
+		static int Get(const TestKey& key)
+		{
+			return key.intKey2;
+		}
+	};
+
+	template<>
+	struct Getter<2>
+	{
+		static const std::string Get(const TestKey& key)
+		{
+			return key.strKey;
+		}
+	};
+};
+
+void TestLeveledHashmap()
+{
+	using namespace geng::lib;
+	using namespace geng::lib::map_helpers;
+
+	LeveledHashmap<std::string, 
+				   int, int, std::string>  lmap;
+
+	TestKey kTest;
+	kTest.intKey1 = 10;
+	kTest.intKey2 = 12;
+	kTest.strKey = "bob";
+
+	auto insVal = Insert(lmap, kTest);
+
+	insVal.first->second = "employee";
+
+	auto itCVal = Find(const_cast<const decltype(lmap)&>(lmap), kTest);
+	auto itVal = Find(lmap, kTest);
+
+	auto iterMap = [](int intKey1, int intkKey2, const std::string& strKey, const std::string& strValue)
+	{
+		return true;
+	};
+
+	IterateChildren(lmap, iterMap);
 }
